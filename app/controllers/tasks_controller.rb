3ffbd32e720d616f608689770365f732bd442288
Thibaud Vuitton <vuittont@gmail.com>
2018-02-13 14:41:06 +0100

    update ok

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,13 +9,24 @@ class TasksController < ApplicationController
   end
 
   def new
-  @task = Task.new
+    @task = Task.new
   end
 
   def create
     @task = Task.create(task_params)
     redirect_to tasks_path
   end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task = @task.update(task_params)
+    redirect_to tasks_path
+  end
+
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # INDEX
   get "tasks", to: "tasks#index", as:"tasks"
-  #SHOW ONE
-
-  get "tasks/new", to: "tasks#new", as:"new_task"
+  patch "tasks/:id", to: "tasks#update"
+  get "tasks/:id/edit", to: "tasks#edit", as:"edit_task"
+  get "tasks/new", to: "tasks#new", as:"task_new"
   post "tasks", to: "tasks#create"
-  get "tasks/:id", to: "tasks#show", as:"task_show"
+  get "tasks/:id", to: "tasks#show", as:"task"
 
 end
